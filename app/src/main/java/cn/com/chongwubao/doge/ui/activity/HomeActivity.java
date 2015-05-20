4b1c3edf4ca3f4c846b9d6c365413504709023bf
lsyiverson <lsyiverson@gmail.com>
2015-05-20 21:39:40 +0800

    [lsyiverson] - Modify drawer arrow color to white

--- app/src/main/java/cn/com/chongwubao/doge/ui/activity/HomeActivity.java
+++ app/src/main/java/cn/com/chongwubao/doge/ui/activity/HomeActivity.java
@@ -1,11 +1,13 @@
 package cn.com.chongwubao.doge.ui.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.TypedValue;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.view.View;
 
 import butterknife.InjectView;
 import cn.com.chongwubao.doge.R;
@@ -18,6 +20,8 @@ public class HomeActivity extends BaseActivity {
     @InjectView(R.id.drawer_layout)
     protected DrawerLayout drawerLayout;
 
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +32,48 @@ public class HomeActivity extends BaseActivity {
         int color = typedValue.data;
 
         drawerLayout.setStatusBarBackgroundColor(color);
+
+        createDrawerToggle();
+
+        drawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    private void createDrawerToggle() {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+        };
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
